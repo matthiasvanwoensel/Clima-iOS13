f8993326f3ec2a5871ff7ff64431f440466aa90b
matthiasvanwoensel <matthias@fluvio.be>
2022-06-27 16:05:00 +0200

    switch

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -44,12 +44,36 @@ struct WeatherManager{
         
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.weather[0].description)
+            let id = decodedData.weather[0].id
+            let weather = getConditionName(conditionID: id)
         }catch{
             print(error)
         }
         
       
+    }
+    
+    func getConditionName(conditionID: Int) -> String{
+        
+        switch conditionID {
+                case 200...232:
+                    return "cloud.bolt"
+                case 300...321:
+                    return "cloud.drizzle"
+                case 500...531:
+                    return "cloud.rain"
+                case 600...622:
+                    return "cloud.snow"
+                case 701...781:
+                    return "cloud.fog"
+                case 800:
+                    return "sun.max"
+                case 801...804:
+                    return "cloud.bolt"
+                default:
+                    return "cloud"
+                }
+    
     }
     
 
